4429367f3954c81d263f406d228444c26ef7f57b
mauda <mauda@mauda.com.br>
2018-08-05 19:57:58 -0300

    Teste para a operação de conflitos no mesmo arquivo

--- cientificos/src/main/java/br/com/mauda/seminario/cientificos/model/Instituicao.java
+++ cientificos/src/main/java/br/com/mauda/seminario/cientificos/model/Instituicao.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 //comentario para testes de conflito entre arquivos diferentes - novo teste
 
+//comentario para testes de conflito no mesmo arquivo - Eclipse
+
 public class Instituicao implements Serializable {
 
     private static final long serialVersionUID = 997084310847650620L;
