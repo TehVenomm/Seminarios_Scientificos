5b1bebbbb8efddc63e68224f7983bf8212617561
Everson Mauda <mauda@mauda.com.br>
2020-07-29 22:36:00 -0300

    Removendo import não necessário

--- cientificos/src/test/java/br/com/mauda/seminario/cientificos/junit/tests/TesteAcaoComprarSobreInscricao.java
+++ cientificos/src/test/java/br/com/mauda/seminario/cientificos/junit/tests/TesteAcaoComprarSobreInscricao.java
@@ -10,7 +10,6 @@ import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.converter.ConvertWith;
