0a7e36f1ae6fc1387d722c48c9f1f2033f9ac172
Gabriel Braz e Santos <gabrielbrazs@gmail.com>
2020-09-17 17:32:23 -0300

    Revert "Correção code smell do sonar cloud"
    This reverts commit 0975dab19ce6c54c4ddeab86e9885c67f9a7afed.

--- cientificos/src/main/java/br/com/mauda/seminario/cientificos/bc/InscricaoBC.java
+++ cientificos/src/main/java/br/com/mauda/seminario/cientificos/bc/InscricaoBC.java
@@ -11,15 +11,6 @@ public class InscricaoBC extends PatternCrudBC<Inscricao> {
 
     private static InscricaoBC instance = new InscricaoBC();
 
-    private static String ER0003 = "ER0003";
-    private static String ER0041 = "ER0041";
-    private static String ER0042 = "ER0042";
-    private static String ER0043 = "ER0043";
-    private static String ER0044 = "ER0044";
-    private static String ER0045 = "ER0045";
-    private static String ER0046 = "ER0046";
-    private static String ER0047 = "ER0047";
-
     private InscricaoBC() {
         // Vazio
     }
@@ -30,23 +21,23 @@ public class InscricaoBC extends PatternCrudBC<Inscricao> {
 
     public void comprar(Inscricao inscricao, Estudante estudante, Boolean direitoMaterial) {
         if (inscricao == null) {
-            throw new SeminariosCientificosException(ER0003);
+            throw new SeminariosCientificosException("ER0003");
         }
 
         if (direitoMaterial == null) {
-            throw new SeminariosCientificosException(ER0041);
+            throw new SeminariosCientificosException("ER0041");
         }
 
         if (inscricao.getSituacao() != SituacaoInscricaoEnum.DISPONIVEL) {
-            throw new SeminariosCientificosException(ER0042);
+            throw new SeminariosCientificosException("ER0042");
         }
 
         if (inscricao.getSeminario().getData().before(new Date())) {
-            throw new SeminariosCientificosException(ER0043);
+            throw new SeminariosCientificosException("ER0043");
         }
 
         if (estudante == null) {
-            throw new SeminariosCientificosException(ER0003);
+            throw new SeminariosCientificosException("ER0003");
         }
 
         estudante.validateForDataModification();
@@ -55,15 +46,15 @@ public class InscricaoBC extends PatternCrudBC<Inscricao> {
 
     public void cancelarCompra(Inscricao inscricao) {
         if (inscricao == null) {
-            throw new SeminariosCientificosException(ER0003);
+            throw new SeminariosCientificosException("ER0003");
         }
 
         if (inscricao.getSituacao() != SituacaoInscricaoEnum.COMPRADO) {
-            throw new SeminariosCientificosException(ER0044);
+            throw new SeminariosCientificosException("ER0044");
         }
 
         if (inscricao.getSeminario().getData().before(new Date())) {
-            throw new SeminariosCientificosException(ER0045);
+            throw new SeminariosCientificosException("ER0045");
         }
 
         inscricao.cancelarCompra();
@@ -71,15 +62,15 @@ public class InscricaoBC extends PatternCrudBC<Inscricao> {
 
     public void realizarCheckIn(Inscricao inscricao) {
         if (inscricao == null) {
-            throw new SeminariosCientificosException(ER0003);
+            throw new SeminariosCientificosException("ER0003");
         }
 
         if (inscricao.getSituacao() != SituacaoInscricaoEnum.COMPRADO) {
-            throw new SeminariosCientificosException(ER0046);
+            throw new SeminariosCientificosException("ER0046");
         }
 
         if (inscricao.getSeminario().getData().before(new Date())) {
-            throw new SeminariosCientificosException(ER0047);
+            throw new SeminariosCientificosException("ER0047");
         }
 
         inscricao.realizarCheckIn();
