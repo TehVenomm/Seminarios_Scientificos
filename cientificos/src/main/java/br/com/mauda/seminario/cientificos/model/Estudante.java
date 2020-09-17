7e708fc9d045e3288578230947ce80e9e999b02a
Gabriel Braz e Santos <gabrielbrazs@gmail.com>
2020-09-17 17:45:15 -0300

    Correção Estudante

--- cientificos/src/main/java/br/com/mauda/seminario/cientificos/model/Estudante.java
+++ cientificos/src/main/java/br/com/mauda/seminario/cientificos/model/Estudante.java
@@ -75,28 +75,24 @@ public class Estudante implements DataValidation {
 
     @Override
     public void validateForDataModification() {
-        if (StringUtils.isBlank(this.email) || this.email.length() > 50) {
-
+        if (this.email.length() > 50 || StringUtils.isBlank(this.email)) {
             throw new SeminariosCientificosException("ER0030");
         }
 
-        if (!this.email.matches(EmailUtils.EMAIL_PATTERN)) { // Regex...
-
+        if (!this.email.matches(EmailUtils.EMAIL_PATTERN)) {
+            // Regex...
             throw new SeminariosCientificosException("ER0030");
         }
 
-        if (StringUtils.isBlank(this.nome) || this.nome.length() > 50) {
-
+        if (this.nome.length() > 50 || StringUtils.isBlank(this.nome)) {
             throw new SeminariosCientificosException("ER0031");
         }
 
-        if (StringUtils.isBlank(this.telefone) || this.telefone.length() > 15) {
-
+        if (this.telefone.length() > 15 || StringUtils.isBlank(this.telefone)) {
             throw new SeminariosCientificosException("ER0032");
         }
 
         if (this.instituicao == null) {
-
             throw new SeminariosCientificosException("ER0003");
         }
 
