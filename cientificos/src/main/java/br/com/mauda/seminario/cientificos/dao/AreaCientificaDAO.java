4284076cbebb05e9616f40fe1a0b3d28afa9b72e
Gabriel Braz <gabrielbrazs@gmail.com>
2020-10-21 23:53:13 +0000

    AreaCientificaDAO.java edited online with Bitbucket

--- cientificos/src/main/java/br/com/mauda/seminario/cientificos/dao/AreaCientificaDAO.java
+++ cientificos/src/main/java/br/com/mauda/seminario/cientificos/dao/AreaCientificaDAO.java
@@ -1,5 +1,7 @@
 package br.com.mauda.seminario.cientificos.dao;
 
+import org.hibernate.Hibernate;
+
 import br.com.mauda.seminario.cientificos.model.AreaCientifica;
 
 public class AreaCientificaDAO extends PatternCrudDAO<AreaCientifica> {
@@ -17,5 +19,6 @@ public class AreaCientificaDAO extends PatternCrudDAO<AreaCientifica> {
 
     @Override
     public void inicializaLazyObjects(AreaCientifica areaCientifica) {
+        Hibernate.initialize(areaCientifica);
     }
 }
